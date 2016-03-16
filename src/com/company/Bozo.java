675a76c640c60e2ae968238394911c52143f75fc
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2016-03-16 12:39:41 -0300

    Renomeacao da classe Main para Bozo

---
+++ src/com/company/Bozo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Bozo {
+
+    /**
+     * Jogo de Bozó para dois jogadores.
+     * @param args sem uso
+     */
+    public static void main(String[] args) {
+
+    }
+}
